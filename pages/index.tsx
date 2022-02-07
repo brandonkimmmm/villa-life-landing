d51fc586013bc785099aaa3d3b852a874e73a5c7
brandonkimmmm <bkim2490@gmail.com>
2022-02-07 13:05:04 +0900

    basic layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,26 @@
 import type { NextPage } from 'next';
 import Countdown from '../components/Countdown';
 import SubscribeForm from '../components/SubscribeForm';
+import { MailIcon } from '@heroicons/react/outline';
 
 const Home: NextPage = () => {
 	return (
-		<div className='flex flex-col h-screen'>
-			<h1>Villa Life</h1>
+		<div className='flex flex-col h-screen space-y-24 justify-center w-96'>
+			<h1 className='font-semibold text-6xl'>Villa Life</h1>
 			<Countdown />
-			<h3>Healthcare is changing with time</h3>
+			<h2 className='font-semibold text-2xl'>
+				Healthcare is changing with time
+			</h2>
 			<p>
 				It is going to be more sophisticated and simple. Stay tuned.
 				Coming soon.
 			</p>
 			<SubscribeForm />
-			<div>EMAIL</div>
+			<div className='flex flex-col'>
+				<span className='inline-flex'>
+					<MailIcon className='h-4' /> hello@stayhiup.com
+				</span>
+			</div>
 		</div>
 	);
 };
