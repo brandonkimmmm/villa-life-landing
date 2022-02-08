557f7ea602dfbd3c760d934631b6db8960b40f72
brandonkimmmm <bkim2490@gmail.com>
2022-02-08 23:45:37 +0900

    add progress circle in countdown

--- components/Countdown.tsx
+++ components/Countdown.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import { RELEASE_DATE } from '../constants';
 import ReactCountdown, { CountdownRenderProps } from 'react-countdown';
+import {
+	CircularProgressbarWithChildren,
+	buildStyles
+} from 'react-circular-progressbar';
 
 const renderer = ({
 	days,
@@ -17,34 +21,77 @@ const renderer = ({
 				<h1 className='font-semibold text-2xl md:text-4xl'>
 					Coming on {RELEASE_DATE.toFormat('LLL dd, yyyy')}
 				</h1>
-				<div className='columns-4 text-3xl md:text-5xl'>
-					<div className='flex flex-col space-y-1 items-center'>
-						<span className='font-semibold'>
-							{days < 10 ? `0${days}` : days}
-						</span>
-						<span className='text-sm uppercase'>days</span>
-					</div>
-					<div className='flex flex-col space-y-1 items-center'>
-						<span className='font-semibold'>
-							{hours < 10 ? `0${hours}` : hours}
-						</span>
-						<span className='text-sm uppercase'>hrs</span>
-					</div>
-					<div className='flex flex-col space-y-1 items-center'>
-						<span className='font-semibold'>
-							{minutes < 10 ? `0${minutes}` : minutes}
-						</span>
-						<span className='text-sm uppercase'>mins</span>
-					</div>
-
-					<div className='flex flex-col space-y-1 items-center'>
-						<span
-							suppressHydrationWarning={true}
-							className='font-semibold'
+				<div className='columns-4 text-xl md:text-5xl gap-2 md:gap-3'>
+					<div className='w-20 md:w-28'>
+						<CircularProgressbarWithChildren
+							value={days}
+							maxValue={100}
+							strokeWidth={4}
+							styles={buildStyles({
+								pathColor: '#469BD4'
+							})}
 						>
-							{seconds < 10 ? `0${seconds}` : seconds}
-						</span>
-						<span className='text-sm uppercase'>secs</span>
+							<div className='flex flex-col space-y-1 items-center'>
+								<span className='font-semibold'>
+									{days < 10 ? `0${days}` : days}
+								</span>
+								<span className='text-sm uppercase'>days</span>
+							</div>
+						</CircularProgressbarWithChildren>
+					</div>
+					<div className='w-20 md:w-28'>
+						<CircularProgressbarWithChildren
+							value={hours}
+							maxValue={24}
+							strokeWidth={4}
+							styles={buildStyles({
+								pathColor: '#469BD4'
+							})}
+						>
+							<div className='flex flex-col space-y-1 items-center'>
+								<span className='font-semibold'>
+									{hours < 10 ? `0${hours}` : hours}
+								</span>
+								<span className='text-sm uppercase'>hrs</span>
+							</div>
+						</CircularProgressbarWithChildren>
+					</div>
+					<div className='w-20 md:w-28'>
+						<CircularProgressbarWithChildren
+							value={minutes}
+							maxValue={60}
+							strokeWidth={4}
+							styles={buildStyles({
+								pathColor: '#469BD4'
+							})}
+						>
+							<div className='flex flex-col space-y-1 items-center'>
+								<span className='font-semibold'>
+									{minutes < 10 ? `0${minutes}` : minutes}
+								</span>
+								<span className='text-sm uppercase'>mins</span>
+							</div>
+						</CircularProgressbarWithChildren>
+					</div>
+					<div className='w-20 md:w-28'>
+						<CircularProgressbarWithChildren
+							value={seconds}
+							maxValue={60}
+							strokeWidth={4}
+							styles={buildStyles({
+								pathColor: '#469BD4'
+							})}
+						>
+							<div className='flex flex-col space-y-1 items-center'>
+								<span
+									suppressHydrationWarning={true}
+									className='font-semibold'
+								>
+									{seconds < 10 ? `0${seconds}` : seconds}
+								</span>
+								<span className='text-sm uppercase'>secs</span>
+							</div>
+						</CircularProgressbarWithChildren>
 					</div>
 				</div>
 			</div>
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import type { AppProps } from 'next/app';
 import '../styles/globals.css';
 import Layout from '../components/Layout';
+import 'react-circular-progressbar/dist/styles.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
 	return (
